f813645e3d0597a6a6c07b06b8de2b79b0b7d7ee
Daniel Pavel <daniel.pavel19@gmail.com>
2024-07-29 15:09:17 +0300

    eslint rules refactor

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,16 +1,14 @@
-import { FC, useState } from 'react'
-import styles from '../styles/PingButton.module.css'
+import { FC, useState } from "react";
+import styles from "../styles/PingButton.module.css";
 
 export const PingButton: FC = () => {
+  const onClick = () => {
+    console.log("Ping!");
+  };
 
-    const onClick = () => {
-        console.log('Ping!')
-    }
-    
-	return (
-		<div className={styles.buttonContainer} onClick={onClick}>
-			<button className={styles.button}>Ping!</button>
-		</div>
-	)
-}
-
+  return (
+    <div className={styles.buttonContainer} onClick={onClick}>
+      <button className={styles.button}>Ping!</button>
+    </div>
+  );
+};
